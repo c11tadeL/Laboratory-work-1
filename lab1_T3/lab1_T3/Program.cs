77edd3195914f111bcb41a498bc07eae550e2b66
OleksandrPopilevych <119813015+c11tadeL@users.noreply.github.com>
2024-02-20 00:47:04 +0300

    Add Task 3

---
+++ lab1_T3/lab1_T3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab1_T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 + 1/2 + 1/4 + 1/8 + 1/16 + ...
+            double series1Sum = SeriesCalculator.CalculateSeriesSum(SeriesCalculator.Series1Term, 1e-6);
+            Console.WriteLine($"Sum of series 1: {series1Sum}");
+
+            // 1 + 1/2! + 1/3! + 1/4! + 1/5! + ...
+            double series2Sum = SeriesCalculator.CalculateSeriesSum(SeriesCalculator.Series2Term, 1e-6);
+            Console.WriteLine($"Sum of series 2: {series2Sum}");
+
+            // -1 + 1/2 - 1/4 + 1/8 - 1/16 + ...
+            double series3Sum = SeriesCalculator.CalculateSeriesSum(SeriesCalculator.Series3Term, 1e-6);
+            Console.WriteLine($"Sum of series 3: {series3Sum}");
+        }
+    }
+}
---
+++ lab1_T3/lab1_T3/SeriesCalculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace lab1_T3
+{
+    public delegate double SeriesTerm(int n);
+    class SeriesCalculator
+    {
+        public static double CalculateSeriesSum(SeriesTerm termDelegate, double precision)
+        {
+            double sum = 0.0;
+            double termValue = termDelegate(0);
+            int n = 0;
+
+            while (Math.Abs(termValue) >= precision)
+            {
+                sum += termValue;
+                n++;
+                termValue = termDelegate(n);
+            }
+
+            return sum;
+        }
+
+        static double Factorial(int n)
+        {
+            double result = 1.0;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        public static double Series1Term(int n)
+        {
+            return 1.0 / Math.Pow(2, n);
+        }
+
+        public static double Series2Term(int n)
+        {
+            return 1.0 / Factorial(n + 1);
+        }
+
+        public static double Series3Term(int n)
+        {
+            return Math.Pow(-1, n + 1) * 1.0 / Math.Pow(2, n);
+        }
+
+
+    }
+}
